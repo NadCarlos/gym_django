0104914504b72144da318d18831804f0ddf2f7a7
NadCarlos <c.nadalig@itecriocuarto.org.ar>
2024-09-02 16:59:09 -0300

    polished styles a bit and finished with the filters

--- gym/administracion/filters.py
+++ gym/administracion/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django import forms
 
 from administracion.models import Paciente, Asistencia
 
@@ -12,16 +13,20 @@ class PacienteFilter(django_filters.FilterSet):
             'apellido',
             'id_obra_social',
             'id_estado_civil',
-            'id_sexo'
+            'id_sexo',
             }
         
 
 class AsistenciasFilter(django_filters.FilterSet):
-    fecha = django_filters.CharFilter(lookup_expr='icontains')
+    fecha = django_filters.DateFilter(
+        widget=forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Select a date','type': 'date'}),
+        )
+
+    id_prestacion_paciente__id_paciente__apellido = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Asistencia
         fields = {
             'fecha',
-            'hora',
+            'id_prestacion_paciente__id_paciente__apellido',
             }
