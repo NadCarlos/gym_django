3e3b2baf7fd208206442871f9710bde0916574bf
NadCarlos <c.nadalig@itecriocuarto.org.ar>
2024-09-04 23:24:30 -0300

    asistencias filter fecha desde hasta

--- gym/administracion/filters.py
+++ gym/administracion/filters.py
@@ -15,19 +15,12 @@ class PacienteFilter(django_filters.FilterSet):
             'id_estado_civil',
             'id_sexo',
             }
-        
 
 class AsistenciasFilter(django_filters.FilterSet):
-
-    fecha = django_filters.DateRangeFilter()
-
+    fecha = django_filters.DateFromToRangeFilter()
 
     id_prestacion_paciente__id_paciente__apellido = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Asistencia
-        fields = {
-            'fecha',
-            'id_prestacion_paciente__id_paciente__apellido',
-            }
-        
+        fields = ['fecha', 'id_prestacion_paciente__id_paciente__apellido']
--- gym/administracion/views/asistencias.py
+++ gym/administracion/views/asistencias.py
@@ -13,17 +13,53 @@ from entrada.repositories.asistencia import AsistenciaRepository
 asisteniaRepo = AsistenciaRepository()
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
-class AsistenciasList(ListView):
+class AsistenciasList(View):
     queryset = asisteniaRepo.get_all()
     template_name = 'asistencias/list.html'
     context_object_name = 'asistencias'
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        self.filterset = AsistenciasFilter(self.request.GET, queryset=queryset)
-        return self.filterset.qs
+    def get(self, request):
+        asistencias = self.queryset
+        return render (
+            request,
+            self.template_name,
+            dict(
+                asistencias=asistencias,
+            )
+        )
+
+class AsistenciasList(View):
+    template_name = 'asistencias/list.html'
+    context_object_name = 'asistencias'
+
+    def get(self, request):
+        # Instanciar el filtro con los datos enviados por el formulario
+        filterset = AsistenciasFilter(request.GET, queryset=asisteniaRepo.get_all())
+
+        # Obtener el queryset filtrado
+        asistencias=filterset.qs
+        return render(
+            request,
+            self.template_name,
+            {
+                'asistencias': asistencias,  # Pasamos las asistencias filtradas
+                'form': filterset.form,  # Pasamos el formulario del filtro al template
+            }
+        )
     
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+
+
+"""    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['form'] = self.filterset.form
-        return context
+        return context"""
+    
+"""
+def post(self, request, *args, **kwargs):
+    armo 1 form que:
+    recibir las 2 fechas
+    retornar:
+    qs.filter(fecha__range(finicio, ffin))
+    qs.filter(fecha__gte=finicio, fecha__lt)
+
+"""
