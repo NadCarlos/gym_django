787da4dab73c369a4061689801147eff446a415d
NadCarlos <c.nadalig@itecriocuarto.org.ar>
2024-09-06 14:26:20 -0300

    in asistencias added obra social and prestacion filters

--- gym/administracion/filters.py
+++ gym/administracion/filters.py
@@ -1,8 +1,7 @@
 import django_filters
 from django import forms
 
-from administracion.models import Paciente, Asistencia
-
+from administracion.models import Paciente, Asistencia, ObraSocial, Prestacion
 
 class PacienteFilter(django_filters.FilterSet):
     apellido = django_filters.CharFilter(lookup_expr='icontains')
@@ -17,10 +16,21 @@ class PacienteFilter(django_filters.FilterSet):
             }
 
 class AsistenciasFilter(django_filters.FilterSet):
-    fecha = django_filters.DateFromToRangeFilter()
 
+    fecha = django_filters.DateFromToRangeFilter()
     id_prestacion_paciente__id_paciente__apellido = django_filters.CharFilter(lookup_expr='icontains')
+    id_prestacion_paciente__id_obra_social = django_filters.ModelChoiceFilter(
+        queryset = ObraSocial.objects.filter(activo=True)
+    )
+    id_prestacion_paciente__id_prestacion = django_filters.ModelChoiceFilter(
+        queryset = Prestacion.objects.filter(activo=True)
+    )
 
     class Meta:
         model = Asistencia
-        fields = ['fecha', 'id_prestacion_paciente__id_paciente__apellido']
+        fields = [
+            'fecha',
+            'id_prestacion_paciente__id_paciente__apellido',
+            'id_prestacion_paciente__id_obra_social',
+            'id_prestacion_paciente__id_prestacion'
+        ]
