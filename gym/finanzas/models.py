b8bf8a98e248704f363aa9f8a32c804e8eb39802
NadCarlos <c.nadalig@itecriocuarto.org.ar>
2025-01-31 12:34:51 -0300

    added models: OrenPago, Descuento, Concepto, DetalleOrden

--- gym/finanzas/models.py
+++ gym/finanzas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Beneficiario(models.Model):
@@ -81,3 +82,124 @@ class Factura(models.Model):
 
     def __str__(self):
         return  self.numero   
+
+
+class OrdenPago(models.Model):
+
+    fecha = models.DateField(
+        null=False,
+        blank=False,
+        verbose_name="fecha"
+    )
+
+    numero = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="numero"
+        )
+
+    id_beneficiario = models.ForeignKey(
+        Beneficiario,
+        on_delete=models.RESTRICT,
+        null=False,
+        blank=False,
+        related_name='id_beneficiario_ordenpago',
+    )
+
+    total = models.DecimalField(
+        verbose_name='total',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    activo = models.BooleanField(
+        default=1,
+        null=False,
+        blank=False,
+        )
+    
+    momento_de_carga = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        blank=False,
+        )
+    
+    id_usuario = models.ForeignKey(
+        User,
+        on_delete=models.RESTRICT,
+        related_name='usuario_ordenpago',
+    )
+
+    def __str__(self):
+        return  self.id_beneficiario.id   
+
+
+class Concepto(models.Model):
+
+    nombre = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="nombre"
+        )
+
+    activo = models.BooleanField(
+        default=1,
+        null=False,
+        blank=False,
+        )
+    
+    def __str__(self):
+        return  self.nombre
+
+
+class Descuento(models.Model):
+
+    id_concepto = models.ForeignKey(
+        Concepto,
+        on_delete=models.RESTRICT,
+        related_name='id_concepto_dto',
+    )
+
+    id_ordenpago = models.ForeignKey(
+        OrdenPago,
+        on_delete=models.RESTRICT,
+        related_name='id_ordenpago_dto',
+    )
+
+    importe = models.DecimalField(
+        verbose_name='importe',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    observaciones = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name="Obseraciones",
+        )
+    
+
+class DetalleOrden(models.Model):
+
+    id_ordenpago = models.ForeignKey(
+        OrdenPago,
+        on_delete=models.RESTRICT,
+        related_name='id_ordenpago_detalleorden',
+    )
+
+    id_factura = models.ForeignKey(
+        Beneficiario,
+        on_delete=models.RESTRICT,
+        null=False,
+        blank=False,
+        related_name='id_factura_detalleorden',
+    )
+
+    importe = models.DecimalField(
+        verbose_name='importe',
+        max_digits=10,
+        decimal_places=2,
+    )
