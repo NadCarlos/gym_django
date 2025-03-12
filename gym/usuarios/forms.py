bea6e789c307d6f3ff963e5278f6a7d2f93f448f
NadCarlos <c.nadalig@itecriocuarto.org.ar>
2025-03-12 11:46:47 -0300

    added group select in register form

--- gym/usuarios/forms.py
+++ gym/usuarios/forms.py
@@ -2,37 +2,40 @@ from django import forms
 from django.contrib.auth import (
     authenticate,
 )
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
 
 class UserRegisterForm(UserCreationForm):
-
     email = forms.EmailField(required=True, help_text='Email Requerido')
+    grupo = forms.ModelChoiceField(
+        queryset=Group.objects.all(),
+        required=True,
+        empty_label="Selecciona un grupo"
+    )
 
     class Meta:
         model = User
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'grupo']
 
-        fields = [
-            'username', 'email', 'password1', 'password2', 'first_name', 'last_name'
-        ]
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('Email ya registrado.')
+        return email
 
-        def clean_email(self):
-            email = self.cleaned_data.get('email')
-            email_exist = User.objets.filter(email=email).exists()
-            if email_exist:
-                raise ValidationError('Email ya registrado.')
-            return email
-        
-        def save(self, commit=True):
-            user= super().save(commit=False)
-            user.email = self.cleaned_data.get('email')
-            user.is_staff = True
-            user.is_superuser = False
-            if commit:
-                user.save()
-            return user
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data.get('email')
+        user.is_staff = False
+        user.is_superuser = False
+        if commit:
+            user.save()
+            grupo = self.cleaned_data.get('grupo')
+            if grupo:
+                user.groups.add(grupo)
+        return user
 
 
 class LoginForm(forms.Form):
