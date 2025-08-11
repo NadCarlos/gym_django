3447b4b4dc128a27a38f80b79d9ed105bb50662d
Carlos Andres Nadalig <c.nadalig@itecriocuarto.org.ar>
2025-08-11 12:18:37 -0300

    paciente_rehabilitacion, derivador and estado_certificado models added

--- gym/rehabilitacion/admin.py
+++ gym/rehabilitacion/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from rehabilitacion.models import(
+    EstadoCertificado,
+    Derivador,
+    PacienteRehabilitacion,
+)
+
+
+@admin.register(EstadoCertificado)
+class EstadoCertificadoAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre',
+    )
+
+
+@admin.register(Derivador)
+class DerivadorAdmin(admin.ModelAdmin):
+    list_display = (
+        'nombre',
+    )
+
+
+@admin.register(PacienteRehabilitacion)
+class PacienteRehabilitacionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id_paciente_area',
+    )
--- gym/rehabilitacion/models.py
+++ gym/rehabilitacion/models.py
@@ -1,3 +1,141 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from administracion.models import (
+    PacienteArea,
+)
+
+
+class EstadoCertificado(models.Model):
+
+    nombre = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+        verbose_name="Nombre_estado_cert",
+    )
+
+    activo = models.BooleanField(
+        default=1,
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return  self.nombre
+    
+
+class Derivador(models.Model):
+
+    nombre = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="Nombre_derivador",
+    )
+
+    activo = models.BooleanField(
+        default=1,
+        null=False,
+        blank=False,
+    )
+
+    id_usuario = models.ForeignKey(
+        User,
+        on_delete=models.RESTRICT,
+        related_name='usuario_derivador',
+    )
+
+    def __str__(self):
+        return  self.nombre
+
+
+class PacienteRehabilitacion(models.Model):
+    
+    id_paciente_area = models.ForeignKey(
+        PacienteArea,
+        on_delete=models.RESTRICT,
+        related_name='id_paciente_area',
+    )
+
+    nombre_tutor = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name="nombre_tutor",
+    )
+
+    celular_tutor = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="celular_tutor",
+    )
+
+    hijos = models.IntegerField(
+        null=False,
+        blank=False,
+    )
+
+    id_estado_certificado = models.ForeignKey(
+        EstadoCertificado,
+        on_delete=models.RESTRICT,
+        related_name='id_estado_certificado'
+    )
+
+    vencimiento_certificado = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='vencimiento_certificado',
+    )
+
+    fecha_junta = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='fecha_junta',
+    )
+
+    ven_presupuesto = models.BooleanField(
+        default=0,
+        null=False,
+        blank=False,
+    )
+
+    vencimiento_presupuesto = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='vencimiento_presupuesto',
+    )
+
+    id_derivador = models.ForeignKey(
+        Derivador,
+        on_delete=models.RESTRICT,
+        related_name="id_derivador"
+    )
+
+    puerto_esperanza = models.BooleanField(
+        default=0,
+        null=False,
+        blank=False,
+    )
+
+    momento_de_carga = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+
+    id_usuario = models.ForeignKey(
+        User,
+        on_delete=models.RESTRICT,
+        related_name='usuario_pac_rehabilitacion',
+    )
+
+    activo = models.BooleanField(
+        default=1,
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return  self.id_paciente_area.id_area.nombre
