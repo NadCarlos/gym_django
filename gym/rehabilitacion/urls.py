278ff5a184f715b7b4fed10d04f03e67a41047ba
Nadalig Carlos <c.nadalig@itecriocuarto.org.ar>
2026-01-21 14:23:22 -0300

    agenda_rehab_paciente shows prestacion instead professional

--- gym/rehabilitacion/urls.py
+++ gym/rehabilitacion/urls.py
@@ -85,7 +85,7 @@ pacientes = [
     path(route='pacientes/to_csv/<int:id>',view=PacienteRehabToCsv.as_view(), name='paciente_rehab_to_csv'),
     path(route='pacientes/altas_to_csv/<int:id>',view=PacienteAltasToCsv.as_view(), name='paciente_altas_rehab_to_csv'),
     path(route='pacientes/redirect_from_existent',view=PacienteRehabRedirectFromExistent.as_view(), name='paciente_rehab_redirect_from_existent'),
-    path(route='pacientes/bulk_add',view=PacientesRehabBulkAdd.as_view(), name='bulk_add'),
+    #path(route='pacientes/bulk_add',view=PacientesRehabBulkAdd.as_view(), name='bulk_add'),
     path(route='pacientes/delete/<int:id>',view=PacienteRehabDelete.as_view(), name='paciente_rehab_delete'),
 ]
 
