89db44e7cfcb7ccbd6fc2e9576b57a72881481a1
Nadalig Carlos <c.nadalig@itecriocuarto.org.ar>
2026-01-27 11:29:55 -0300

    fixed hardcode in daily_agenda_teorica

--- gym/rehabilitacion/management/commands/daily_agenda_teorica.py
+++ gym/rehabilitacion/management/commands/daily_agenda_teorica.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rehabilitacion.repositories.asistencia_teorica import AsistenciaRehabRepository
 from rehabilitacion.repositories.agenda_rehab import AgendaRehabRepository
 
@@ -7,8 +9,14 @@ agendaRehabRepo = AgendaRehabRepository()
 
 
 def daily_agenda_teorica_func():
-    agendas = agendaRehabRepo.filter_by_dia(id_dia=1)
-    for agenda in agendas:
-        asistenciaTeoricaRepo.create(
-            id_agenda_rehab=agenda
-        )
+    try:
+        dias = [1,2,3,4,5]
+        today = date.today()
+        dia = dias[today.weekday()]
+        agendas = agendaRehabRepo.filter_by_dia(id_dia=dia)
+        for agenda in agendas:
+            asistenciaTeoricaRepo.create(
+                id_agenda_rehab=agenda
+            )
+    except:
+        pass
